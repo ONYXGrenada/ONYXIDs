d21152a0fbf5c5830fe5aae69b0d3de9a6d69494
Lenus Walker <walker.lenus@gmail.com>
2018-02-12 10:25:30 -0400

    Login Activity Update

--- app/src/main/java/com/onyxgrenada/onyxids/LoginActivity.kt
+++ app/src/main/java/com/onyxgrenada/onyxids/LoginActivity.kt
@@ -1,39 +1,42 @@
 package com.onyxgrenada.onyxids
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.content.Intent
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import com.google.android.gms.auth.api.Auth
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_login.*
 
-class MainActivity : AppCompatActivity(){
-
-    /*private val REQUEST_CODE_SIGN_IN = 1234
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity : AppCompatActivity(), View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    private val REQUEST_CODE_SIGN_IN = 1234
     private val TAG = "GoogleSignIn"
     private val WEB_CLIENT_ID = "724744964375-u6q98njkfo15egcu537qv3kuereberoa.apps.googleusercontent.com"
 
     private var mGoogleApiClient: GoogleApiClient? = null
-    private var mAuth: FirebaseAuth? = null*/
-
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val displayName = intent.getStringExtra(EXTRA_MESSAGE)
-        txt_welcome_text.text = "Welcome $displayName"
-
-        /*btn_google_sign_in.setOnClickListener(this)
+        setContentView(R.layout.activity_login)
+        // Set up the login form.
+        btn_google_sign_in.setOnClickListener(this)
 
         // Example of a call to a native method
         //txt_sample_text.text = stringFromJNI()
@@ -48,10 +51,9 @@ class MainActivity : AppCompatActivity(){
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build()
 
-        mAuth = FirebaseAuth.getInstance()*/
-
+        mAuth = FirebaseAuth.getInstance()
     }
-    /*
+
     override fun onStart() {
         super.onStart()
         val currentUser = mAuth!!.currentUser
@@ -103,7 +105,6 @@ class MainActivity : AppCompatActivity(){
                 }
     }
 
-
     override fun onConnectionFailed(connectionResult: ConnectionResult) {
         Log.e(TAG, "onConnectionFailed():" + connectionResult)
         Toast.makeText(applicationContext, "Google Play Services error.", Toast.LENGTH_SHORT).show()
@@ -112,39 +113,34 @@ class MainActivity : AppCompatActivity(){
     private fun signIn() {
         val intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
         startActivityForResult(intent, REQUEST_CODE_SIGN_IN)
-        Toast.makeText(applicationContext, "Seems like we are having an error.",
-                Toast.LENGTH_SHORT).show()
     }
-
-
 
     private fun updateUI(user: FirebaseUser?) {
         if (user != null) {
-            txt_sample_text.text = "Google User email: " + user.email!! + user.uid
-            //tvDetail.text = "Firebase User ID: " + user.uid
 
-            btn_google_sign_in.visibility = View.GONE
-            //layout_sign_out_and_disconnect.visibility = View.VISIBLE
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            intent.putExtra(EXTRA_MESSAGE ,user.displayName)
+            startActivity(intent)
         } else {
-            txt_sample_text.text = "Signed Out"
+            txt_login_text.visibility = View.VISIBLE
+            txt_login_text.text = "Signed Out"
             //tvDetail.text = null
 
             btn_google_sign_in.visibility = View.VISIBLE
             //layout_sign_out_and_disconnect.visibility = View.GONE
         }
     }
-    */
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    //external fun stringFromJNI(): String
 
     companion object {
 
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
-        }
+        /**
+         * Id to identity READ_CONTACTS permission request.
+         */
+
+
+        /**
+         * A dummy authentication store containing known user names and passwords.
+         * TODO: remove after connecting to a real authentication system.
+         */
     }
 }
